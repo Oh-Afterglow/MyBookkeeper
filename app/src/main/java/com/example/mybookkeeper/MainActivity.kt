84e5c688cb1aee0baa18dc4f0a774ce05443e13f
Oh-Afterglow <wendy_grignard@163.com>
2022-05-27 10:56:02 +0800

    2 buttons

--- app/src/main/java/com/example/mybookkeeper/MainActivity.kt
+++ app/src/main/java/com/example/mybookkeeper/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.mybookkeeper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onClickOfAdd(v: View) {
+        val intent = Intent(this, AddBookActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun onClickOfView(v: View) {
+        val intent = Intent(this, BookListActivity::class.java)
+        startActivity(intent)
     }
 }
