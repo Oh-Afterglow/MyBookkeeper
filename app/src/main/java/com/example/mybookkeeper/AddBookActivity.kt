c2e454218732cb894a94ec60bb04016fc59236c8
Oh-Afterglow <wendy_grignard@163.com>
2022-05-27 23:28:28 +0800

    2 activities & request

--- app/src/main/java/com/example/mybookkeeper/AddBookActivity.kt
+++ app/src/main/java/com/example/mybookkeeper/AddBookActivity.kt
@@ -1,11 +1,92 @@
 package com.example.mybookkeeper
 
+import android.app.AlertDialog
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.fragment.app.DialogFragment
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+
 
 class AddBookActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_book)
+
+        initAddBookButton()
+        initSearchButton()
+    }
+
+    private fun initAddBookButton() {
+        val addBookButton: Button = findViewById(R.id.button2)
+        addBookButton.isEnabled = false
+    }
+
+    private fun initSearchButton() {
+        val searchButton = findViewById<Button>(R.id.button)
+        searchButton.setOnClickListener{
+            searchBook()
+        }
+    }
+
+    private fun searchBook() {
+        val searchBox = findViewById<EditText>(R.id.isbnSearchBox)
+        val isbn = searchBox.text.toString()
+        val apikey = "your_api_key"  // Visit https://jike.xyz/jiekou/isbn.html to get your own API key
+        val url = "https://api.jike.xyz/situ/book/isbn/$isbn?apikey=$apikey"
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                val data = response.getJSONObject("data")
+                if (data.isNull("name")) {
+                    NoResultDialogFragment().show(supportFragmentManager, "NoResultDialogFragment")
+                } else {
+                    val title = data.getString("name")
+                    val subname = if (data.isNull("subname")) "" else data.getString("subname")
+                    val author = data.getString("author")
+                    val translator = if (data.isNull("translator")) "" else data.getString("translator")
+                    val publisher = data.getString("publishing")
+                    val publishDate = data.getString("published")
+                    val price = data.getString("price")
+                    val photoUrl = data.getString("photoUrl")
+                    val authorIntro = data.getString("authorIntro")
+                    val description = data.getString("description")
+                    val pages = data.getString("pages")
+                    val titleText = findViewById<TextView>(R.id.textView3)
+                    titleText.text = title
+                    val authorText = findViewById<TextView>(R.id.textView4)
+                    authorText.text = author
+                    val publishInfo = findViewById<TextView>(R.id.textView5)
+                    val publishInfoText = "$publisher · $publishDate"
+                    publishInfo.text = publishInfoText
+                }
+            },
+            { error ->
+                println(error)
+            }
+        )
+        val queue = Volley.newRequestQueue(this)
+        queue.add(jsonObjectRequest)
+    }
+}
+
+class NoResultDialogFragment : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(activity)
+        builder.setTitle("QwQ")
+        builder.setMessage("Book not found.")
+        builder.setPositiveButton("OK") { dialog, _ ->
+            val searchBox = activity?.findViewById<EditText>(R.id.isbnSearchBox)
+            searchBox?.text?.clear()
+            dialog.dismiss()
+        }
+        return builder.create()
     }
 }
--- app/src/main/java/com/example/mybookkeeper/MainActivity.kt
+++ app/src/main/java/com/example/mybookkeeper/MainActivity.kt
@@ -3,21 +3,23 @@ package com.example.mybookkeeper
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    fun onClickOfAdd(v: View) {
-        val intent = Intent(this, AddBookActivity::class.java)
-        startActivity(intent)
-    }
+        val buttonAdd = findViewById<Button>(R.id.button_add)
+        buttonAdd.setOnClickListener {
+            val intent = Intent(this, AddBookActivity::class.java)
+            startActivity(intent)
+        }
 
-    fun onClickOfView(v: View) {
-        val intent = Intent(this, BookListActivity::class.java)
-        startActivity(intent)
+        val buttonView = findViewById<Button>(R.id.button_view)
+        buttonView.setOnClickListener {
+            val intent = Intent(this, BookListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
